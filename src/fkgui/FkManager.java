38c8c1a9eea344e6acece0533713efe912c37d8a
Jimmy Christensen <dusted@dusted.dk>
2014-08-11 19:20:39 +0200

    Removed unused libs and fixed warning.

--- src/fkgui/FkManager.java
+++ src/fkgui/FkManager.java
@@ -2,7 +2,6 @@ package fkgui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Vector;
